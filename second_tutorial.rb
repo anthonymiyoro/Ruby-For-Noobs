4e9f7cf6aec94235c4d0355fd778fef313ecfe52
anthonymiyoro <amiyoro@gmail.com>
2015-08-10 16:35:40 +0300

    Added some stuff to the second tutorial

--- second_tutorial.rb
+++ second_tutorial.rb
@@ -11,6 +11,21 @@ puts(name)
 
 #We can also convert an integer to a string
 eighteen = "10".to_i + "8".to_i
-puts(eighteen)
+puts(eighteen);
 
+#We can also append strings together as shown below
+cool_things = "Chips, " + "chocolate and " + "choppers"
+puts(cool_things);
 
+#The above is called concatation
+
+#Perhaps we can have a practical demonstration
+
+#ask for a number
+puts("I will add 10 to any number! Give me a number!!");
+
+#collect the number from the user
+input = gets
+
+#add 5 to the number the add 10
+puts(10 + input.to_i)
