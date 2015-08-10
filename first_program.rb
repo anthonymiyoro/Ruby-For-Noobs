0f688136d6f9ff7ae57afee49bef4de215da99d2
anthonymiyoro <amiyoro@gmail.com>
2015-08-10 14:49:23 +0300

    Fixed some of my grammar and added new maths functions

--- first_program.rb
+++ first_program.rb
@@ -10,9 +10,15 @@ puts(3 * 5)
 #The slash means divide
 puts(8 / 2)
 
-#O.M.G Where is the remainder?
+#O.M.G Where are the remainders?
 puts(9 / 2)
 
 #Here they are!
 puts(9.0 / 2)
 puts(9.0 / 2.0)
+
+#OR
+#Number of times divided
+puts(11 / 4)
+#Remainder of dividing 11 by 4 shown here
+puts(11 % 4)
