abc88528de4af1cb4ffa4cca74169b46da6a2039
anthonymiyoro <amiyoro@gmail.com>
2015-08-10 16:52:21 +0300

    completed second tutorial

--- second_tutorial.rb
+++ second_tutorial.rb
@@ -28,4 +28,10 @@ puts("I will add 10 to any number! Give me a number!!");
 input = gets
 
 #add 5 to the number the add 10
-puts(10 + input.to_i)
+puts(10 + input.to_i);
+
+
+#We can also convert a number to a string
+one = 1.to_s
+three = 3.to_s
+puts(one + three + " is an unlucky number");
