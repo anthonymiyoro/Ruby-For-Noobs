821bd616b505e98c30873c499a4f3f263463a43a
anthonymiyoro <amiyoro@gmail.com>
2015-08-10 14:41:52 +0300

    First Commit

---
+++ first_program.rb
@@ -0,0 +1,18 @@
+puts("The answer to life, the universe and everything!!")
+puts(42)
+
+puts ("Maths begins below")
+puts(3 + 5)
+
+#The asterisk means multiply
+puts(3 * 5)
+
+#The slash means divide
+puts(8 / 2)
+
+#O.M.G Where is the remainder?
+puts(9 / 2)
+
+#Here they are!
+puts(9.0 / 2)
+puts(9.0 / 2.0)
