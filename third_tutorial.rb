ad5464ad7d5bfd3016fb92c57b160e19268e5be4
anthonymiyoro <amiyoro@gmail.com>
2015-08-10 17:30:14 +0300

    Added gets.chomp method

---
+++ third_tutorial.rb
@@ -0,0 +1,13 @@
+# THE CHOMP METHOD
+
+#The program below will output the "!" character on a new line
+#This is because it interprates the "Enter"key as well
+puts("Type in your name!")
+name = gets
+puts("Hello " + name + "!")
+
+#Not to worry though! 
+#If we replace the gets method with gets.chomp we are able to strip of the character at the end.
+puts("Type in your name!")
+name = gets.chomp
+puts("Hello " + name + "!")
