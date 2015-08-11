d6938bda2f437743a895c5544fdb877425aeffbd
anthonymiyoro <amiyoro@gmail.com>
2015-08-11 19:50:25 +0300

    Added an array tutorial and completed looping tutorial

---
+++ Arrays.rb
@@ -0,0 +1,15 @@
+#ARRAYS
+#Arrays can store different types of data as shown below
+cool_things = ["Thinkpads","Ferraris","Fighter Jets"]
+even_numbers = [2, 4, 6, 8]
+empty_array = []
+
+
+#To get something out of an array we use its index
+cool_things = ["Thinkpads","Ferraris","Fighter Jets"]
+puts(cool_things[0]);
+puts(cool_things[1]);
+puts(cool_things[2]);
+
+
+#Rather than repeating methods we can use the array's length method
--- looping.rb
+++ looping.rb
@@ -5,7 +5,7 @@ puts("Input a number!")
 num = gets.to_i
 
 #Below is the loop
-#Runs when a number is less than 100
+#Runs when a number is less than 100 (Until the tese num < 100 runs false)
 while num < 100
   puts("That number is less than 100! Keep trying!")
   
@@ -15,3 +15,21 @@ end
 
 #Only runs at the end of the loop
 puts("You typed " + num.to_s + " which is at least 100!")
+
+
+#Below is a somewhat simpler example
+#It will print "Hello world!" thrice
+i = 0
+while i < 3
+  puts("Hello world!")
+  i = i + 1
+end
+
+puts("All done!")
+
+#NOTE
+#avoid infinite loops like below
+
+# while 1 == 1
+  # puts("We're trapped in a loop!")
+# end
