24c7c0b03aa7cf0cf84d74896b3279c003c017f2
anthonymiyoro <amiyoro@gmail.com>
2015-08-11 17:40:19 +0300

    Completed looping tutorial

--- looping.rb
+++ looping.rb
@@ -4,6 +4,7 @@
 puts("Input a number!")
 num = gets.to_i
 
+#Below is the loop
 #Runs when a number is less than 100
 while num < 100
   puts("That number is less than 100! Keep trying!")
@@ -12,4 +13,5 @@ while num < 100
   num = gets.to_i
 end
 
+#Only runs at the end of the loop
 puts("You typed " + num.to_s + " which is at least 100!")
