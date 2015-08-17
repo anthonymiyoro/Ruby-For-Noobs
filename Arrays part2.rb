f75a3514c30ba8c17f3c7c233c5219d1e5a5dc2b
anthonymiyoro <amiyoro@gmail.com>
2015-08-17 11:40:33 +0300

    Added third arrays tutorial and whilre statement tutorial

--- Arrays part2.rb
+++ Arrays part2.rb
@@ -31,3 +31,34 @@ end
 # If you type in "Racecars", "Lasers", and "Aeroplanes", then:
 #    cool_things == ["Racecars", "Lasers", "Aeroplanes"]
 # because each subsequent item is pushed onto the end of the array.
+
+
+#We can also try and add things to the beginning of an array using the UNSHIFT method
+    
+cool_things = []
+
+while cool_things.length < 3
+  puts("Tell me a cool thing!")
+  a_cool_thing = gets.chomp
+  
+  #adds 1 item at the beginning of the array
+  cool_things.unshift(a_cool_thing)
+end
+
+# Now if you type in "Racecars", "Lasers", and "Aeroplanes", then:
+#    cool_things == ["Aeroplanes", "Laser", "Racecars"]
+# because each subsequent item is `unshift`ed onto the front of the
+# array.
+
+
+puts("Here's some cool things in backward order:")
+
+# Loop through and print each of the cool things.
+idx = 0
+while idx < cool_things.length
+  puts(cool_things[idx])
+  idx = idx + 1
+end
+
+# This prints the array out in order of increasing position, which is
+# the opposite of the order the items were typed in.
---
+++ Arrays part3.rb
@@ -0,0 +1,6 @@
+#We can remove objects from  arrays using shift and pop 
+
+arr = [2, 3, 5, 7]
+
+# prints 2, the item at position zero
+puts(arr[0])
