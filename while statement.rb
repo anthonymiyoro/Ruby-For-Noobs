12bdc0ef224f6bdd8447cae995213e01123be1d9
anthonymiyoro <amiyoro@gmail.com>
2015-08-17 12:03:52 +0300

    Completed while statement tutorial and created while modifier tutorial

---
+++ while statement.rb
@@ -0,0 +1,21 @@
+#In this tutorial we will cover while statements
+#When using while statements code is executed while certain conditions are true
+
+i = 0
+num = 5
+
+
+while i < num do
+ puts("Inside the loop i = #{i}")
+   i +=1
+ end  
+
+#The code above will run as long as i is less than 5 a
+#The output will be as below
+
+Inside the loop i = 0
+Inside the loop i = 1
+Inside the loop i = 2
+Inside the loop i = 3
+Inside the loop i = 4
+
