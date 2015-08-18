60be34d49f51cb720ddb5411400606ee31872c89
anthonymiyoro <amiyoro@gmail.com>
2015-08-18 12:40:01 +0300

    Added Standard Classes turorial

---
+++ Standard-Classes.rb
@@ -0,0 +1,24 @@
+# Standard classes such as the math module help you solve problems easily
+#To get the square root of 400 we can:
+
+Math.sqrt(400)
+
+# Which will then give us
+
+=> 20.0
+
+# What if you wanted to calculate what specific day July 4th occurred in 2008? 
+# You can use Ruby's built-in Time class.
+
+2008-07-04 00:00:00 -0400
+ t.monday?
+ 
+ #Which will then give you;
+ 
+ false
+ 
+ t.friday?
+ 
+ #Which will then give you;
+ 
+ true
