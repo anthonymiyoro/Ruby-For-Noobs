acd4145db9b9e1866c98236914e52674fca01ee7
anthonymiyoro <amiyoro@gmail.com>
2015-08-20 14:15:17 +0300

    Gave the dogs names

--- classes.rb
+++ classes.rb
@@ -12,16 +12,19 @@ end
 
 
 #The < sign makes GermanShepherd a subclass of Dog
-class GermanShepherd < Dog
-  def howl
-    puts "wooooooooooo!"
-  end
-end
+# class GermanShepherd < Dog
+  # def howl
+     # puts "wooooooooooo!"
+   # end
+#   
+   # def initialize(aName, aHowl)
+ # end
+# end
 
 # We then create dog objects below
 
-mydog = Dog.new
-yourdog = Dog.new
+mydog = Dog.new('Simba')
+yourdog = Dog.new('Potty')
 
 # We can then assign the objects to the talk function
 
@@ -29,10 +32,10 @@ mydog.talk
 yourdog.talk
 
 #The gshp object can then be initialized below
-gshp = GermanShephered.new("Scotty")
+ # gshp = GermanShephered.new('Scotty')
 
 #The talk method can then be called from the dog class
-gshp.talk
+ # gshp.talk
 
 #The howl method can also be called from the GermanShepherd subclass
-gshp.howl
+ # gshp.howl
