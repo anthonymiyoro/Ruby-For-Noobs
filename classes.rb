7ad7b205502bf959c7aa456c4f1946877ec65f11
anthonymiyoro <amiyoro@gmail.com>
2015-08-20 13:32:47 +0300

    Initialised object from 2 different classes

--- classes.rb
+++ classes.rb
@@ -28,4 +28,11 @@ yourdog = Dog.new
 mydog.talk
 yourdog.talk
 
-#The german shepherd class can then be initialized
+#The gshp object can then be initialized below
+gshp = GermanShephered.new("Scotty")
+
+#The talk method can then be called from the dog class
+gshp.talk
+
+#The howl method can also be called from the GermanShepherd subclass
+gshp.howl
